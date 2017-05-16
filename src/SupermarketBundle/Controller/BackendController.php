8550947796a716daaa2bf99d056491dc4ab1ed76
Quentin Lemaire <quentin.lemaire10@supinfo.com>
2017-05-16 02:03:53 +0200

    search complete for command and user

--- src/SupermarketBundle/Controller/BackendController.php
+++ src/SupermarketBundle/Controller/BackendController.php
@@ -62,29 +62,55 @@ class BackendController extends Controller {
 		return $this->redirectToRoute('backend');
 	}
 
-	public function searchAction(Request $request){
+	public function searchUserAction(Request $request){
 		$name = $request->query->get('query');
 		$em = $this->getDoctrine()->getManager();
-		/*return $this->render('SupermarketBundle:Admin:search.html.twig', array(
-			'users' => $users,
-		));*/
 
 		$RAW_QUERY = "SELECT * FROM user WHERE 
 						username LIKE '%".$name."%'
-						OR delivery LIKE '%".$name."%'
+						OR last_name LIKE '%".$name."%'
 						OR first_name LIKE '%".$name."%'
 						OR email LIKE '%".$name."%'
 						OR id LIKE '%".$name."%'
 						OR billing LIKE '%".$name."%'
 						OR delivery LIKE '%".$name."%'
-						OR delivery LIKE '%".$name."%'
-						OR delivery LIKE '%".$name."%'
 						OR roles LIKE '%".$name."%'
 						;";
 		$statement = $em->getConnection()->prepare($RAW_QUERY);
 		$statement->execute();
-		return $this->render('SupermarketBundle:Admin:search.html.twig', array(
+		return $this->render('SupermarketBundle:Admin:search_users.html.twig', array(
 			'users' => $result = $statement->fetchAll(),
+		));
+	}
+
+	public function searchReceiptAction(Request $request){
+		$receipt = $request->query->get('query');
+		$em = $this->getDoctrine()->getManager();
+
+		$RAW_QUERY = "SELECT * FROM receipts WHERE 
+						id LIKE '%".$receipt."%'
+						OR user_id LIKE '%".$receipt."%'
+						OR delivery LIKE '%".$receipt."%'
+						OR billing LIKE '%".$receipt."%'
+						OR content LIKE '%".$receipt."%'
+						OR date LIKE '%".$receipt."%'
+						;";
+		$statement = $em->getConnection()->prepare($RAW_QUERY);
+		$statement->execute();
+		$receipts = $statement->fetchAll();
+		foreach ($receipts as &$result){
+			$result['content'] = json_decode($result['content']);
+		}
+		return $this->render('SupermarketBundle:Admin:search_receipt.html.twig', array(
+			'receipts' => $receipts,
+		));
+	}
+
+	public function listUsersAction(){
+		$em = $this->getDoctrine()->getManager();
+		$users = $em->getRepository('SupermarketBundle:User')->findAll();
+		return $this->render('SupermarketBundle:Admin:list_users.html.twig', array(
+			'users' => $users,
 		));
 	}
 
